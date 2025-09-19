993bb591bdea9237bee72f2ca9c1aee07039e1a3
stefanBid <biddau.stefano99@gmail.com>
2025-09-19 21:52:33 +0200

    Add interface and JSON for Button component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,25 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface UiElementsButton extends Struct.ComponentSchema {
+  collectionName: 'components_ui_elements_buttons';
+  info: {
+    displayName: 'Button';
+  };
+  attributes: {
+    icon: Schema.Attribute.String;
+    link: Schema.Attribute.String;
+    size: Schema.Attribute.Enumeration<['small', 'medium']> &
+      Schema.Attribute.DefaultTo<'medium'>;
+    text: Schema.Attribute.String;
+    variant: Schema.Attribute.Enumeration<['white', 'tertiary']> &
+      Schema.Attribute.DefaultTo<'tertiary'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'ui-elements.button': UiElementsButton;
+    }
+  }
+}
