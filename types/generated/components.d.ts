29181a8695eb2e868d0e1002a6e2e0488be0a2a5
stefanBid <biddau.stefano99@gmail.com>
2025-09-20 02:07:00 +0200

    Add JSON and interface for link component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -16,10 +16,23 @@ export interface UiElementsButton extends Struct.ComponentSchema {
   };
 }
 
+export interface UiElementsLink extends Struct.ComponentSchema {
+  collectionName: 'components_ui_elements_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    href: Schema.Attribute.String & Schema.Attribute.DefaultTo<'#'>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'ui-elements.button': UiElementsButton;
+      'ui-elements.link': UiElementsLink;
     }
   }
 }
